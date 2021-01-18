2386474ccbf16783b7c127ef34d8f52810e79989
reivax-boucoi <reivax.boucoi@laposte.net>
2021-01-18 20:22:45 +0100

    added plotted soft

---
+++ Buck/Software/Projet_inter_Buck_Equipe1_Nucleo/Core/Inc/Buck.h
@@ -0,0 +1,73 @@
+#ifndef INC_BUCK_H_
+#define INC_BUCK_H_
+
+#include "stm32l4xx_hal.h"
+#include <stdio.h>
+
+#define F_PWM    150000UL // min : 1220 Hz max: 80 MHz
+#define F_ACQ    5000UL // min : 0.018 Hz max: 80 MHz
+#define KVOUTMON (6.2/(110.0+6.2))
+#define KVINMON  (0.9375*0.05838)//(6.2/(100.0+6.2))
+#define KIMON    (0.006*51.0)
+#define VCC      (3.3)
+
+#define VOUT_MAX 48.0
+#define VOUT_MIN 0.00
+#define IOUT_MAX 10.0
+#define IOUT_MIN 0.00
+#define DUTY_MAX 0.90
+#define DUTY_MIN 0.10
+
+#define REG_MODE_CV   0
+#define REG_MODE_CC   1
+#define REG_MODE_MPPT 2
+#define REG_MODE_OL   3
+
+#define F_TIM1 80000000UL/F_PWM
+
+#define KP_CC 0.1 			// Valeur du coefficient proportionnel régulation CC
+#define KI_CC .001 			// Valeur du coefficient intégral régulation CC
+#define KP_CV 0.01//(250.0/2370.0) 		// Valeur du coefficient proportionnel régulation CV
+#define KI_CV 1 			// Valeur du coefficient intégral régulation CV
+
+#define SAT_ERR_TOT 400 // Valeur pour la saturation de l'erreur totale
+
+int Duty_Cycle;	//Timer duty value, from 0 to F_TIM1
+int Vout_mon;		//Measured output voltage, from 0 to 4095, PC0
+int Vin_mon;		//Measured input  voltage, from 0 to 4095, PC1
+int I_mon;			//Measured output current, from 0 to 4095, PC2
+
+//Onboard blue button : PC13
+//Onboard LED LD2 : PA5
+//PWM Output : PA8
+
+int Pin;   //Measured input power for MPPT regulation
+int Pin_p; //Previously measured input power for MPPT regulation
+int I_inc;	//
+
+int Reg_Mode;
+int Vout_set;
+int Iout_set;
+
+uint32_t ADC_buffer[3];
+
+int Delta_Err;  	//Valeur de l'erreur entre la tension/courant souhaitée et la valeur lue
+int Err_Tot; 		//Pour la partie intégrale de la régulation
+int Delta_Duty; 	//Variation du duty après la correction proportionnelle, intégrale
+
+void Set_Duty_Cycle();
+void SetVout(float target);
+void SetI(float target);
+void Set_Duty_OL(float target);
+
+void RegulateCV(void);
+void RegulateCC(void);
+void RegulateMPPT(void);
+
+float getVout(void);
+float getIout(void);
+float getVin(void);
+
+void printBuckStatus(void);
+
+#endif /* INC_BUCK_H_ */
---
+++ Buck/Software/Projet_inter_Buck_Equipe1_Nucleo/Core/Src/Buck.c
@@ -0,0 +1,112 @@
+#include "Buck.h"
+
+extern TIM_HandleTypeDef htim1;
+
+void Set_Duty_Cycle(){
+
+	if (Duty_Cycle>F_TIM1*DUTY_MAX){
+		Duty_Cycle= F_TIM1*DUTY_MAX;
+	} else if (Duty_Cycle<F_TIM1*DUTY_MIN)	{
+		Duty_Cycle=F_TIM1*DUTY_MIN;
+	}
+	__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,Duty_Cycle);
+}
+
+void SetVout(float target){
+	if(target>VOUT_MAX)target=VOUT_MAX;
+	if(target<VOUT_MIN)target=VOUT_MIN;
+	Vout_set = (target)*(KVOUTMON*4096.0)/VCC;
+}
+
+void SetI(float target){
+	if(target>IOUT_MAX)target=IOUT_MAX;
+	if(target<IOUT_MIN)target=IOUT_MIN;
+	Iout_set = (target)*(KIMON*4096.0)/VCC;
+}
+
+void Set_Duty_OL(float target){
+	if(target>DUTY_MAX)target=DUTY_MAX;
+	if(target<DUTY_MIN)target=DUTY_MIN;
+	Duty_Cycle=(target)*F_TIM1;
+	Set_Duty_Cycle();
+}
+
+void RegulateCV(void){
+	Delta_Err = Vout_set - Vout_mon;
+	/*Err_Tot += Delta_Err;
+
+	if (Err_Tot > SAT_ERR_TOT){
+		Err_Tot = SAT_ERR_TOT;
+	}else if (Err_Tot < -(SAT_ERR_TOT)){
+		Err_Tot = -SAT_ERR_TOT;
+	}
+*/
+	Delta_Duty = (Delta_Err *KP_CV) ;//+ (Err_Tot *KI_CV );
+	Duty_Cycle += Delta_Duty;
+
+	Set_Duty_Cycle();
+}
+
+void RegulateCC(void){
+	Delta_Err = Iout_set - I_mon;
+	/*Err_Tot += Delta_Err;
+
+	if (Err_Tot > SAT_ERR_TOT)
+	{
+		Err_Tot = SAT_ERR_TOT;
+	}
+	if (Err_Tot < -(SAT_ERR_TOT))
+	{
+		Err_Tot = -SAT_ERR_TOT;
+	}*/
+
+	Delta_Duty = (Delta_Err *KP_CC);//+ (Err_Tot * KI_CC);
+	Duty_Cycle += Delta_Duty;
+
+	Set_Duty_Cycle();
+}
+
+void RegulateMPPT(void){
+	/*//Iin=Iout*Vout/Vin
+	Pin = I_mon*Vout_mon;
+	if (Pin < Pin_p){
+		I_inc = -1;
+	}else{
+		I_inc = 1;
+	}
+	Pin_p = Pin;
+	Iout_set = Iout_set + I_inc;*/
+}
+
+
+float getVout(void){
+	return (float)Vout_mon*(VCC/KVOUTMON/4096.0);
+}
+float getIout(void){
+	return (float)I_mon*(VCC/KIMON/4096.0);
+}
+float getVin(void){
+	return (float)Vin_mon*(VCC/KVINMON/4096.0);
+}
+
+void printBuckStatus(void){
+	printf("Vin %.1fV, Vout %.1fV, Iout %.2fA, Duty : %d%%, Regmode ",getVin(), getVout(), getIout(), Duty_Cycle*100/533);
+	switch(Reg_Mode){
+	case REG_MODE_CV :
+		printf("CV");
+		break;
+	case REG_MODE_OL:
+		printf("OL");
+		break;
+	case REG_MODE_CC :
+		printf("CC");
+		break;
+	case REG_MODE_MPPT:
+		printf("MPPT");
+		break;
+	default :
+		break;
+	}
+	printf("\r\n");
+//	printf("\tI_mon : %d\t Vin_mon : %d Err : %d\r\n",I_mon,Vin_mon,Delta_Err);
+}
